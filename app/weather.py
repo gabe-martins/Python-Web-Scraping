1bf52e7f9ba1193444b66e30fd683b01275a690a
Gabriel Martins <gabriel_alves_m@outlook.com.br>
2021-07-15 16:49:25 -0300

    update weather app and requirements

--- app/weather.py
+++ app/weather.py
@@ -1,9 +1,22 @@
 import requests
 from bs4 import BeautifulSoup
 
+
 def scraping(base_url):
   req = requests.get(base_url)
   soup = BeautifulSoup(req.content, 'html.parser')
   return soup
 
-scraping('https://www.climatempo.com.br/previsao-do-tempo/cidade/332/voltaredonda-rj')
+link = 'https://www.climatempo.com.br/previsao-do-tempo/cidade/332/voltaredonda-rj'
+
+print(scraping(link).find(class_ = '-gray -line-height-24 _center').text)
+
+def weather(city):
+  req = requests.get(city)
+  soup = BeautifulSoup(req.content, 'html.parser')
+
+  print('Resumo: ' + soup.find(class_ = '-gray -line-height-24 _center').text)
+  print('Máximas: ' + soup.find(id = 'max-temp-1').string)
+  print('Mínimas: ' + soup.find(id = 'min-temp-1').string)
+
+weather(link)
